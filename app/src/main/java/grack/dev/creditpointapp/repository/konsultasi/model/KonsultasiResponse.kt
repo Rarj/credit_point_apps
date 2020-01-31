174c864e2a16e7ff7db456fb83958612564ab2a6
Developer Arj <developer.arj@gmail.com>
2020-01-31 20:12:54 +0700

    updated

--- app/src/main/java/grack/dev/creditpointapp/repository/konsultasi/model/KonsultasiResponse.kt
+++ app/src/main/java/grack/dev/creditpointapp/repository/konsultasi/model/KonsultasiResponse.kt
@@ -4,7 +4,7 @@ package grack.dev.creditpointapp.repository.konsultasi.model
 import com.google.gson.annotations.SerializedName
 
 data class KonsultasiResponse(
-  @SerializedName("pelanggaranParent")
+  @SerializedName("pelanggaran")
   var pelanggaran: List<Pelanggaran>,
   @SerializedName("status")
   var status: String?
